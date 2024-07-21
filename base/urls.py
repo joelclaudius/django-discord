43946a3a82838cb6f5de75e0858162cb2eb0a582
Claudius Joel <otiiclaudius@gmail.com>
2024-07-21 17:28:36 +0300

    Added models

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.room, name='room')
+    path('room/<str:pk>/', views.room, name='room'),
+
+    path('create-room/', views.createRoom, name='create-room')
 ]
--- base/views.py
+++ base/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
-from .models import Room
+# views.py
 
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
 def home(request):
     rooms = Room.objects.all()
-    context = { 'rooms': rooms}
-    return render(request, 'home.html', context )
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room.objects.get(id=pk)
-    context = {'room':room}
-    return render(request, 'room.html' ,context)
+    room = get_object_or_404(Room, id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
